0af00c362a43b96db29ed924013265da17aa1d0e
Bushra <bushrafaizan1216@gmail.com>
2021-06-21 15:23:30 +0200

    edited award

--- project_webscrapping.py
+++ project_webscrapping.py
@@ -13,7 +13,7 @@ top_250_movie_url = 'http://www.imdb.com/chart/top'
 response = requests.get(top_250_movie_url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-# Parsing movie title, Year, Url, Crew details and movie ratings
+#Parsing movie title, Year, Url, Crew details and movie ratings
 movie_title = [" " .join(tag.text.split()[1:-1]) for tag in soup.find_all('td',{'class':'titleColumn'})]
 year = [int(tag.text.split()[-1][1:-1]) for tag in soup.find_all('td',{'class':'titleColumn'})]
 urls = ['http://www.imdb.com'+ a.get('href') for a in soup.select('td.titleColumn a')]
@@ -22,27 +22,25 @@ movie_ratings = [round(float(a.attrs.get('data-value')),2) for a in soup.select(
 
 # Parsing the awards won or nominated by each movie using the above parsed url list
 awards = []
+i = 1
 for url in urls:
     next_page = requests.get(url)
     another_soup = BeautifulSoup(next_page.text,'html.parser')
     award_got = []
-    for a in another_soup.find('span',{'class':'awards-blurb'}):
-        a = a.string.split()
-        if a:
+    if another_soup.find('span',{'class':'awards-blurb'}):
+        for a in another_soup.find('span',{'class':'awards-blurb'}):
+            a = a.string.split()
             award_got.append(a)
-        else:
-            award_got.append(0)
+    else:
+        award_got.append(["0"])
+    print(i)
+    i = i+1
     if len(award_got)==3:
         awards.append(" ".join(award_got[1]))
     elif len(award_got)==1:
         awards.append(" ".join(award_got[0]))
-    print(awards)
+
 print(awards)
-
-    #if award_got:
-    #    awards.append(' '.join(award_got[1]))
-
-#print(awards)
 
 
 # Getting the no. of top movies in each year through counter
@@ -54,8 +52,10 @@ df = pd.DataFrame(s, columns=['year', 'count'])
 ax = df.plot.bar(x='year', y='count', rot=60)
 plt.show()
 
+
 #No. of movies in top_chart of director
 director = [d.split(',')[0][:-7] for d in crew]
+
 no_of_movies_of_each_director = Counter(director)
 most_movies = no_of_movies_of_each_director.most_common(20)
 d_m = pd.DataFrame(most_movies, columns=['director', 'count'])
@@ -71,13 +71,11 @@ data = pd.DataFrame({'movie_title':movie_title,
                      'Year' : year,
                      'Director':director,
                      'Ratings':movie_ratings,
-                     'crew': crew,
+                     'Movie stars': movie_stars,
                      'Awards': awards,
                      'Url' : urls })
 
 
-#print(data.head())
 print(tabulate(data, headers = 'keys', tablefmt = 'psql'))
 
-#Data types of dataframe
-print(data.dtypes)
+
